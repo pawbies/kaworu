a59a8123c6ed56ed7c54e0016d74388125ef0fc2
pawbies <alex.fuchs@pawbies.net>
2026-01-27 21:59:09 +0100

    day one finished

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -1,9 +1,54 @@
-use crate::cli::ScriptFormat::{self, *};
+use std::{
+    fs::{read_to_string, write},
+    process::exit,
+};
+
+use crate::{
+    cli::ScriptFormat::{self, *},
+    config::Config,
+};
 
 pub fn run(format: ScriptFormat) {
-    match format {
-        Bash => println!("Bash"),
-        Powershell => println!("Powershell"),
-        CMD => println!("CMD"),
+    let file_contents = read_to_string("kaworu.toml").unwrap_or_else(|e| {
+        eprintln!("Error: {}", e);
+        exit(1);
+    });
+
+    let config = match Config::from_toml(file_contents) {
+        Ok(config) => config,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            exit(1);
+        }
+    };
+
+    let script: String = match format {
+        Bash => config
+            .items
+            .iter()
+            .map(|item| item.bash_line() + "\n")
+            .collect(),
+        Powershell => config
+            .items
+            .iter()
+            .map(|item| item.ps_line() + "\n")
+            .collect(),
+        CMD => config
+            .items
+            .iter()
+            .map(|item| item.cmd_line() + "\n")
+            .collect(),
+    };
+    let file_name = match format {
+        Bash => "kaworu.sh",
+        Powershell => "kaworu.ps",
+        CMD => "kaworu.cmd",
+    };
+    match write(file_name, script) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            exit(1);
+        }
     }
 }
--- src/item.rs
+++ src/item.rs
@@ -13,4 +13,26 @@ impl Item {
     pub fn from(name: String, src: PathBuf, dest: PathBuf) -> Self {
         Self { name, src, dest }
     }
+
+    pub fn bash_line(&self) -> String {
+        format!(
+            "ln -s {} {} #{}",
+            self.src.to_str().expect("Couldn't output path"),
+            self.dest.to_str().expect("Couldn't output path"),
+            self.name
+        )
+    }
+
+    pub fn ps_line(&self) -> String {
+        format!(
+            "New-Item -ItemType SymbolicLink -Path {} -Target {} #{}",
+            self.src.to_str().expect("Couldn't output path"),
+            self.dest.to_str().expect("Couldn't output path"),
+            self.name
+        )
+    }
+
+    pub fn cmd_line(&self) -> String {
+        "Still Testing".to_string()
+    }
 }
